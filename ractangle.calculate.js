a27aa5d14f14ddc1d1a145d53a94f427f53d2592
rakibulVSC <mrakibulhasan755@gmail.com>
2024-02-13 10:26:56 +0600

    add rectangle area

--- ractangle.calculate.js
+++ ractangle.calculate.js
@@ -1,3 +1,20 @@
 function calculateRectangleArea(){
-console.log('connect')
+// get length the rectangle
+const widthInput=document.getElementById('rectangle-width');
+const widthText=widthInput.value;
+const width=parseFloat(widthText);
+console.log(width);
+// get length 
+const lengthInput=document.getElementById('rectangle-length');
+const lengthText=lengthInput.value;
+const length=parseFloat(lengthText);
+console.log(length);
+
+// get rectangle area 
+const area=width*length;
+console.log('the rectangle area is:',area)
+// display rectangle area 
+const areaSpan=document.getElementById('rectangle-area');
+areaSpan.innerText=area;
+
 }
